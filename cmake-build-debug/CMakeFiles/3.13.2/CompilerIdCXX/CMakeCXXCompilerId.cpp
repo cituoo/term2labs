a84e44e5b1534db32e32ecdede43c6ce590f90a1
cituoo <cituoo@yandex.ru>
2019-06-25 01:56:07 +0300

    miss space

--- cmake-build-debug/CMakeFiles/3.13.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
+++ cmake-build-debug/CMakeFiles/3.13.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
@@ -541,7 +541,7 @@ char const info_version_internal[] = {
   'n', '_',
   'i', 'n', 't', 'e', 'r',
   'n', 'a', 'l', '[',
-  COMPILER_VERSION_INTERNAL,']', '\0'};
+  COMPILER_VERSION_INTERNAL, ']', '\0'};
 #endif
 
 /* Construct a string literal encoding the version number components. */
